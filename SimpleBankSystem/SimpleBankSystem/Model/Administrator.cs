78654dd51e64f58cb58d5f0f32fc3480ad4b061e
Jordyz96 <Jordyz96@hotmail.com>
2018-03-16 19:16:16 +1100

    Included the Administrator class as it was under an untracked change on Visual Studios version control

---
+++ SimpleBankSystem/SimpleBankSystem/Model/Administrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBankSystem.Model
+{
+    class Administrator : Person
+    {
+        public Administrator() : base('A') //'A' denotes Administrator user
+        {
+
+        }
+    }
+}
